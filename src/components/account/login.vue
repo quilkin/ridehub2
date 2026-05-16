c649ecf9eb395c1ed45940da47a2bf9369764ed4
quilkin <quilkin2@gmail.com>
2026-05-16 09:17:45 +0100

    update version number; add DBMemorial hint

--- src/components/account/login.vue
+++ src/components/account/login.vue
@@ -4,7 +4,7 @@
  * Log in to a user's Ridehub account
  */
 
-import { ref, onBeforeMount} from 'vue'
+import { ref, onBeforeMount, onMounted} from 'vue'
 // @ts-ignore
 import { nameRules,pwRules} from '../../utils/rules'
 import { myFetch } from '../../utils/fetch'
@@ -18,7 +18,9 @@ const loginForm = ref();
 const userOrEmail = ref('');
 const password = ref('');
 const showPass = ref(false);
-//const remember = ref(false);
+const passwordField = ref();
+const showDBMemorial = ref(false);
+
 
 const emit = defineEmits(['loggedIn','signUp','forgotPass','guestVisit'])
 
@@ -26,10 +28,26 @@ onBeforeMount(() => {
   const storedUsername = window.localStorage.getItem('username');
   if (storedUsername) {
     userOrEmail.value = storedUsername;
-    //remember.value = true;
+
+  }
+  const today = new Date();
+  //todo: ideally these dates would come from the server so they can be updated without an app update
+  const memorialStart = new Date('2026-05-01');
+  const memorialEnd = new Date('2026-06-13');
+  if (today >= memorialStart && today <= memorialEnd) {
+    showDBMemorial.value = true;
   }
 })
+onMounted(() => {
+  // Force a click on the password field to trigger browser autofill
+  setTimeout(() => {
+    const input = passwordField.value?.$el?.querySelector('input');
+    if (input) {
+      input.click();
 
+    }
+  }, 100);
+})
 async function submit() {
   const {valid} = await loginForm.value?.validate()
   if (valid) {
@@ -38,7 +56,8 @@ async function submit() {
     //if (remember.value) {
       window.localStorage.setItem('username', username);
     //} else {
-    //  window.localStorage.removeItem('username');
+    // for existing installations
+     window.localStorage.removeItem('password');
     //}
     let user = new User(username, pass);
         
@@ -91,17 +110,20 @@ function forgot() {
 <template>
   <div class="d-flex align-center flex-column">
   <v-card >
+     <v-card-text class="pa-2" v-if="showDBMemorial">
+      <div class="text-center">Don't forget to enter the <a href="https://tinyurl.com/DBMR2026" target="_blank">DB Memorial Ride</a>.</div>
+    </v-card-text>
     <v-card-title class="headline black" primary-title>
       Sign in to TCC RideHub
     </v-card-title>
     <v-card-text class="pa-2">
+   
       <v-form @submit.prevent="submit" ref="loginForm">
         <v-text-field v-model="userOrEmail" name="username" autocomplete="username" :rules="nameRules" autocapitalize="off"
          label="User name or email" hint="Username will be 3 to 10 characters">
         </v-text-field>
-        <v-text-field v-model="password" name="password" autocomplete="current-password" :append-inner-icon="showPass ? mdiEye : mdiEyeOff"
+        <v-text-field ref="passwordField" v-model="password" name="password" autocomplete="current-password" :append-inner-icon="showPass ? mdiEye : mdiEyeOff"
           @click:append-inner="showPass = !showPass"   :type="showPass ? 'text' : 'password'" 
-          hint="Password will be at least 6 characters"
           :rules="pwRules"  label="Password">
         </v-text-field>
 
@@ -112,6 +134,9 @@ function forgot() {
         <v-btn color="blue" variant="outlined" @click="forgot()" block class="mt-2">      Forgot password?   </v-btn>
 
       </v-form>
+    </v-card-text>
+    <v-card-text class="pa-2">
+      <div class="text-center">&copy 2021-6 Version 2.3.0 <a href="https://truro.cc" target="_blank">Learn more about TCC</a>.</div>
     </v-card-text>
   </v-card>
   </div>
